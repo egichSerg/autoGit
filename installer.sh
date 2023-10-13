5ebc9edb551a2f5039a930711e751cc35b17b269
egichSerg <125789871+egichSerg@users.noreply.github.com>
2023-10-13 15:13:18 +0700

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#create directory
+cd ~/ && mkdir scripts && cd scripts
+
+#download files and make them executables
+curl https://raw.githubusercontent.com/egichSerg/autoGit/main/autogit.sh -o autogit.sh && chmod +x autogit.sh
+curl https://raw.githubusercontent.com/egichSerg/autoGit/main/autogitreboot.sh -o autogitreboot.sh && chmod +x autogitreboot.sh
+curl https://raw.githubusercontent.com/egichSerg/autoGit/main/cat.sh -o cat.sh && chmod +x cat.sh
+
+#add them to .bashrc
+cd ..
+echo "#autogit" >> .bashrc
+echo "alias sv='~/scripts/autogit.sh'" >> .bashrc
+echo "alias rb='~/scripts/autogitreboot.sh'" >> .bashrc
+echo "alias cat='~/scripts/cat.sh'" >> .bashrc
